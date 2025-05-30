f05b04c875e3e4dd8418c0ba74ecb21a34d15241
Freja <freja@osterbog.me>
2025-05-30 19:18:39 +0200

    small refactor

--- src/contexts.tsx
+++ src/contexts.tsx
@@ -1,7 +1,5 @@
 import {createContext, type ReactNode} from "react";
-import {Dinosaur} from "./projects/Dinosaur.tsx";
-import {WatchStrap} from "./projects/WatchStrap.tsx";
-import {TableClock} from "./projects/TableClock.tsx";
+import {currentProjects} from "./currentProjects.tsx";
 
 type layoutContext = {
   is_mobile: boolean;
@@ -15,23 +13,6 @@ type ProjectMapType = {
   previewText: string;
   previewImage: string|null;
 }
-
-const currentProjects = [{
-  project: <Dinosaur/>,
-  previewName: 'Dinosaur watering can',
-  previewText: 'Dinosaurs are cool',
-  previewImage: ''
-},{
-  project: <WatchStrap/>,
-  previewName: 'Hand-made leather watchstrap',
-  previewText: '',
-  previewImage: ''
-},{
-  project: <TableClock/>,
-  previewName: 'Table clock',
-  previewText: '',
-  previewImage: ''
-}];
 
 
 export const ProjectContext = createContext<ProjectMapType[]>(currentProjects)
---
+++ src/currentProjects.tsx
@@ -0,0 +1,20 @@
+import {Dinosaur} from "./projects/Dinosaur.tsx";
+import {WatchStrap} from "./projects/WatchStrap.tsx";
+import {TableClock} from "./projects/TableClock.tsx";
+
+export const currentProjects = [{
+  project: <Dinosaur/>,
+  previewName: 'Dinosaur watering can',
+  previewText: 'Dinosaurs are cool',
+  previewImage: ''
+},{
+  project: <WatchStrap/>,
+  previewName: 'Hand-made leather watchstrap',
+  previewText: '',
+  previewImage: ''
+},{
+  project: <TableClock/>,
+  previewName: 'Table clock',
+  previewText: '',
+  previewImage: ''
+}];

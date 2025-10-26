80c373b4c4c80960eacd2d5ba65d99feccd50fe4
Freja <freja.11@live.dk>
2025-10-26 19:59:17 +0100

    new menu

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,25 +1,40 @@
 import './layout.css'
 import {Link, Outlet} from "react-router-dom";
-import {useContext} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {LayoutContext} from "../contexts.tsx";
-
+import { ReactSVG } from 'react-svg'
 
 export const Header = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  useEffect(() => {
+    if (ref.current)
+      setHeaderHeight(ref.current.clientHeight);
+  }, [ref]);
+
   return (
     <header>
-      <h1>
-        <Link to="/">Hello there ✌️</Link>
-      </h1>
-      <div className={"menu-box"}>
+      <div className={"menu-box"} id={"menu"} ref={ref}>
+        <h1>
+          <Link to="/">Hello there ✌️</Link>
+        </h1>
         <div className={"menuitem"}>
           <Link to="/">Home</Link>
         </div>
-        <div className={"menuitem"}>
-          <Link to="/projects">Projects</Link>
+        <div className={"menuitem open-menu-button"} onClick={() => setMenuVisible(!menuVisible)}>
+          Projects
+          <ReactSVG src="./icons/dropdown-arrow.svg" wrapper={'span'}/>
         </div>
         <div className={"menuitem"}>
           <Link to="/about">About</Link>
         </div>
+      </div>
+      <div className={"menu-content"} style={{...{top: headerHeight}, ...(menuVisible? {display: "block"} : {display: "none"})}}>
+        <div><Link to={"/projects"} onClick={() => setMenuVisible(false)}>Projects</Link></div>
+        <div><Link to={"/recipes"} onClick={() => setMenuVisible(false)}>Recipes</Link></div>
+        <div><Link to={"/origami"} onClick={() => setMenuVisible(false)}>Origami</Link></div>
       </div>
     </header>
   )

24b7631d3753d91abbe4ee27f484a87e5af0ac49
Freja <freja@osterbog.me>
2025-11-03 22:41:57 +0100

    nicer css and html

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,13 +1,11 @@
 import './layout.sass'
 import {Link, Outlet} from "react-router-dom";
-import {useContext, useEffect, useRef, useState} from "react";
+import {useContext, useState} from "react";
 import {LayoutContext, ListContext} from "../contexts.tsx";
 import { ReactSVG } from 'react-svg'
 import Hamburger from "hamburger-react";
 
 export const Header = () => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [headerHeight, setHeaderHeight] = useState(0);
   const [mainMenuVisible, setMainMenuVisible] = useState(false);
   const [subMenuVisible, setSubMenuVisible] = useState(false);
   const closeMenus = () => {
@@ -17,38 +15,33 @@ export const Header = () => {
   const theme = useContext(LayoutContext);
   const projectList = useContext(ListContext);
 
-  useEffect(() => {
-    if (ref.current)
-      setHeaderHeight(ref.current.clientHeight);
-  }, [ref]);
+  const mainMenu = (
+    <>
+      <h2 onClick={() => setMainMenuVisible(false)}>
+        <Link to="/">Home</Link>
+      </h2>
+      <h2 onClick={() => setSubMenuVisible(!subMenuVisible)}>
+        <ReactSVG src="./icons/down-arrow.svg" wrapper={'span'}/>
+        Projects
+      </h2>
+      <h2 onClick={() => setMainMenuVisible(false)}>
+        <Link to="/about">About</Link>
+      </h2>
+    </>
+  );
 
-  if (theme.is_mobile) {
-    const mainMenu = (
-      <>
-        <h2 onClick={() => setMainMenuVisible(false)}>
-          <Link to="/">Home</Link>
-        </h2>
-        <h2 onClick={() => setSubMenuVisible(true)}>
-          <ReactSVG src="./icons/down-arrow.svg" wrapper={'span'}/>
-          Projects
-        </h2>
-        <h2 onClick={() => setMainMenuVisible(false)}>
-          <Link to="/about">About</Link>
-        </h2>
-      </>
-    );
+  const subMenu = (
+    <>
+      {projectList.map(({title, type}, key) => (
+        <h2 key={key}><Link to={"/" + type} onClick={() => closeMenus()}>{title}</Link></h2>
+      ))}
+    </>
+  );
 
-    const subMenu = (
-      <>
-        {projectList.map(({title, type}, key) => (
-          <h2 key={key}><Link to={"/" + type} onClick={() => closeMenus()}>{title}</Link></h2>
-        ))}
-      </>
-    );
-
+  if (theme.is_mobile)
     return (
       <header>
-        <div style={{display: "flex", justifyContent: "space-between", margin: "0.5rem 1rem", alignItems: "center"}}>
+        <div className={'mobile-header'}>
           <h1>
             <Link to="/">Hello there ✌️</Link>
           </h1>
@@ -57,33 +50,27 @@ export const Header = () => {
             toggled={mainMenuVisible}
           />
         </div>
-        <div className={"navbar"} style={mainMenuVisible ? { display: "flex" } : {display: "none"}}>
+        <div
+          className={'mobile-item-list'}
+          style={mainMenuVisible ? {display: "flex"} : {display: "none"}}
+        >
           {subMenuVisible ? subMenu : mainMenu}
         </div>
       </header>
     )
-  }
   return (
     <header>
-      <div className={"menu-box"} id={"menu"} ref={ref}>
+      <div className={"desktop-items"}>
         <h1>
           <Link to="/">Hello there ✌️</Link>
         </h1>
-        <div className={"menuitem"}>
-          <Link to="/">Home</Link>
-        </div>
-        <div className={"menuitem open-menu-button"} onClick={() => setSubMenuVisible(!subMenuVisible)}>
-          Projects
-          <ReactSVG src="./icons/down-arrow.svg" wrapper={'span'}/>
-        </div>
-        <div className={"menuitem"}>
-          <Link to="/about">About</Link>
-        </div>
+        {mainMenu}
       </div>
-      <div className={"menu-content"} style={{...{top: headerHeight}, ...(subMenuVisible? {display: "block"} : {display: "none"})}}>
-        {projectList.map(({title, type}, key) => (
-          <h2 key={key}><Link to={"/" + type} onClick={() => setSubMenuVisible(false)}>{title}</Link></h2>
-        ))}
+      <div
+        className={"desktop-sub-items"}
+        style={subMenuVisible? {display: "flex"} : {display: "none"}}
+      >
+        {subMenu}
       </div>
     </header>
   )
